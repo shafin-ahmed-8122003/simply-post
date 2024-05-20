927448a49935e65c14f044498086cf9391807caa
Shafin <shafin.ahmed.8122003@gmail.com>
2024-05-20 11:58:44 +0600

    Landing - About section completed

--- pages/landing/Landing.tsx
+++ pages/landing/Landing.tsx
@@ -1,18 +1,20 @@
 import LoginButton from "@/components/buttons/LoginButton";
 import Logo from "@/components/logo/Logo";
+import About from "./sections/About";
 import Hero from "./sections/Hero";
 import Introduction from "./sections/Introduction";
 
 const Landing = () => {
     return (
         <>
-            <header className="absolute top-0 left-0 w-full p-4 flex justify-between items-center">
+            <header className="absolute top-0 left-0 w-full p-4 flex justify-between items-center bg-base-100">
                 <Logo />
                 <LoginButton />
             </header>
             <main className="snap-y snap-mandatory h-screen overflow-auto">
                 <Hero />
                 <Introduction />
+                <About />
             </main>
         </>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
     ],
     theme: {},
     plugins: [daisyui],
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
     daisyui: {
         themes: [
             {
